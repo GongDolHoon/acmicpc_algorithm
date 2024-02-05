2081a1220a9ab6b2619fbac30b120d3b264cb747
Seunghoon Park <qkrtmdgns23@gmail.com>
2024-02-06 01:28:17 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1 <= N <= 100,000
+int _n;
+
+int _x;
+
+struct cmp
+{
+	bool operator()(int a, int b)
+	{
+		return a > b;	
+	}	
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	// I-1. 첫째 줄에 연산의 개수 N이 주어진다.
+	cin >> _n;
+	
+	priority_queue<int, vector<int>, cmp> pq;
+	
+	for(int i = 0; i < _n; ++i)
+	{
+		// I-2. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다.
+		// I-2-1. 만약 x가 자연수라면 배열에 x라는 값을 넣는 (추가하는) 연산이고,
+		// I-2-2. x가 0이라면 배열에서 가장 작은 값을 출력하고 
+		// 그 값을 배열에서 제거하는 경우이다.
+		cin >> _x;
+		
+		// O-1. 입력에서 0이 주어진 횟수만큼 답을 출력한다.
+		// O-2. 만약 배열이 비어 있는 경우인데 가장 작은 값을 출력하라고 한 경우에는
+		// 0을 출력한다. 	
+		if(_x == 0)
+		{
+			if(pq.empty() == true)
+			{
+				cout << "0\n";		
+			}
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(_x);
+		}
+	}
+	
+	return 0;
+}
