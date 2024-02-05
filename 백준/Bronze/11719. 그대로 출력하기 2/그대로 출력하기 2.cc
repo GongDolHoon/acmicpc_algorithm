a986d5dcbb00d94e93b79a0ef44b3a8589324ee4
Seunghoon Park <qkrtmdgns23@gmail.com>
2024-02-06 01:53:02 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string _s;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	// I-1. 입력이 주어진다.
+	while(getline(cin, _s))
+	{
+		// O-1. 입력받은 그대로를 출력한다. 
+		cout << _s << '\n';
+	}
+	
+	return 0;
+}
