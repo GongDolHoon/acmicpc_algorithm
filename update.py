4cc63c3a57e38e4c545feed99a1edb957898e7cd
Seunghoon Park <qkrtmdgns23@gmail.com>
2024-02-06 02:20:32 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,51 +3,50 @@
 import os
 from urllib import parse
 
-HEADER="""# 
+HEADER="""#
 # 백준 문제 풀이 목록
 """
 
 def main():
-    content = ""
-    content += HEADER
+  content = ""
+  content += HEADER
+  
+  directories = []
+  solveds = []
+  
+  for root, dirs, files in os.walk("."):
+    dirs.sort()
+    if root == '.':
+      for dir in ('.git', '.github'):
+        try:
+          dirs.remove(dir)
+        except ValueError:
+          pass
+      continue
     
-    directories = [];
-    solveds = [];
-
-    for root, dirs, files in os.walk("."):
-        dirs.sort()
-        if root == '.':
-            for dir in ('.git', '.github'):
-                try:
-                    dirs.remove(dir)
-                except ValueError:
-                    pass
-            continue
-
-        category = os.path.basename(root)
+    category = os.path.basename(root)
+    
+    if category == 'images':
+      continue
+      
+    directory = os.path.basename(os.path.dirname(root))
+    
+    if directory == '.':
+      continue
+      
+    if directory not in directories:
+      content += "### {}\n".format(directory)
+      content += "| 문제 | 풀이 |\n"
+      content += "| ----- | ----- |\n"
+      directories.append(directory)
+      
+    for file in files:
+      if category not in solveds:
+        content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+        solveds.append(category)
         
-        if category == 'images':
-            continue
-        
-        directory = os.path.basename(os.path.dirname(root))
-        
-        if directory == '.':
-            continue
-            
-        if directory not in directories:
-            content += "### {}\n".format(directory)
-            content += "| 문제번호 | 링크 |\n"
-            content += "| ----- | ----- |\n"
-            directories.append(directory)
-
-        for file in files:
-            if category not in solveds:
-                content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
-                solveds.append(category)
-                print("category : " + category)
-
-    with open("README.md", "w") as fd:
-        fd.write(content)
-        
+  with open("README.md", "w") as fd:
+    fd.write(content)
+    
 if __name__ == "__main__":
-    main()
+  main()
