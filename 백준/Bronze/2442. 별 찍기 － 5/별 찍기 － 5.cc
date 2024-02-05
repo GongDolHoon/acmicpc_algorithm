27e0a62e135ce4504ba8a58961a75eda9cb20c38
Seunghoon Park <qkrtmdgns23@gmail.com>
2024-02-06 01:29:42 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1 <= N <= 100
+int _n;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	// I-1. 첫째 줄에 N이 주어진다.
+	cin >> _n;
+	
+	// O-1. 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다. 
+	for(int i = 0; i < _n; ++i)
+	{
+		for(int j = 0; j < _n - (i + 1); ++j)
+		{
+			cout << ' ';
+		}
+		
+		for(int j = 0; j < 2 * i + 1; ++j)
+		{
+			cout << '*';
+		}
+		
+		cout << '\n';
+	}
+	
+	return 0;
+}
